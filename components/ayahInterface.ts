f93526e28fc59ddc969e24bad4a65963aa97e47b
ucok23 <ulubis98@gmail.com>
2022-02-13 08:44:42 +0700

    adding ayah interface

---
+++ components/ayahInterface.ts
@@ -0,0 +1,9 @@
+export interface AyahInterface {
+    surah?: string;
+    ayah?: number;
+    isLoading: boolean;
+    arab?: string;
+    translate?: string;
+    audioSrc?: string,
+    handleClick?: () => void
+}
